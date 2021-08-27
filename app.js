3efa682d7cf32ddf1ab13bc4e91d2b7cdc2d6ce4
RAHUL SINGH <singh.rahul3111998vipin@gmail.com>
2021-08-27 16:36:18 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const initialPrice=document.querySelector("#initial-price");
+const stockQuantity=document.querySelector("#stock-quantity");
+const currentPrice=document.querySelector("#current-price");
+const button=document.querySelector("#btn");
+const outputMessage=document.querySelector(".output");
+
+function calculateProfitAndLoss(initial,quantity,current){
+    if(initial>current){
+        var loss=(initial-current)*quantity;
+        var lossPercent=(loss/initial)*100;
+        // console.log(`the loss ocurred is ${loss} and loss percentage is: ${lossPercent}%`);
+        outputMessage.innerText="the loss occurred is: "+loss+" and the percentage is:"+lossPercent.toFixed(2)+"%";
+        outputMessage.style.color="red";
+    }else if(current>initial){
+        var profit=(current-initial)*quantity;
+        var profitPercent=(profit/initial)*100;
+        outputMessage.innerText="the profit gained is: "+profit+" and the percentage is:"+profitPercent.toFixed(2)+"%";
+        outputMessage.style.color="green";
+    }else{
+        outputMessage.innerText="NO profit NO gain";
+    }
+}
+
+function clickListener(){
+    var ip=Number(initialPrice.value);
+    var qty=Number(stockQuantity.value);
+    var curr=Number(currentPrice.value);
+    if(ip&&qty&&curr){
+        calculateProfitAndLoss(ip,qty,curr);
+    }else{
+        outputMessage.innerText="Please enter all the fields";
+    }
+   
+}
+
+button.addEventListener("click",clickListener);
